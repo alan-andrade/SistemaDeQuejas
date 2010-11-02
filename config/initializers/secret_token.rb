c65ba9d427b0183b97b850af16d9f239173a5b01
Alan Andrade <gallito7@gmail.com>
2010-11-02 09:23:12 -0600

    Pushing the project to github.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SistemaDeQuejas::Application.config.secret_token = 'b95878b3fd8e638616c12a4e04351097c29b58f22a81c4b39862c2ba7d8f22cbd8fde9139e71b25e1c93a8236a7f4721fc26046e520cc90e828a5504b800e413'
