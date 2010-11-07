04bd3aa611fcaa86a7881fdd0a28451f174b2ac6
Alan Andrade <gallito7@gmail.com>
2010-11-07 15:56:25 -0600

    Render PDF icon besides a pdf link. Waahoo!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
+  // Flash Message Animation
   if ($('.flash').length > 0){
     $('.flash').slideDown(1000).delay(2500).slideUp(1000)
   };
+  
+  // Little Icons rendering
+    // PDF links
+    var links  = $('a[href$=.pdf]');
+    links.each(function(i){
+      $(this).css({ 'padding-left'        : '20px',
+                    'padding-bottom'      : '5px',
+                    'background-image'    : 'url("../images/icons/page_white_acrobat.png")',
+                    'background-position' : 'top-left',
+                    'background-repeat'   : 'no-repeat' });
+    });
 });
