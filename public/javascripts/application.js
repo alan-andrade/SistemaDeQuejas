b5d273d17bfc463171b55e6535753ad368a08e96
Alan Andrade <gallito7@gmail.com>
2010-11-06 18:41:26 -0600

    No big changes. Neither advance.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   if ($('.flash').length > 0){
-    $('.flash').slideDown(1000).delay(1500).slideUp(1000)
+    $('.flash').slideDown(1000).delay(2500).slideUp(1000)
   };
 });
