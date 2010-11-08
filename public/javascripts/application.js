9ecb45568d7a23a142e4771f774f696af6668177
Alan Andrade <gallito7@gmail.com>
2010-11-08 12:26:46 -0600

    FIXED: jQuery split and join method to get rid of more than 1 digit numbers problem. :D

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -25,34 +25,38 @@ $(document).ready(function(){
       }      
   });
   
+    function add_value_to_cookie(cookie_name, object){
+      if (cookie_defined(cookie_name) == true){     // Cookie Exists?
+          values  = $.cookie(cookie_name).split(' ');  // values to_array
+          values.push(object.value);                  // push value
+          values  = $.unique(values);                 // remove repetition
+          $.cookie(cookie_name, values.join(' '));
+      } else{                                     // Initialize Cookie
+        $.cookie(cookie_name, object.value);      
+      }
+    };
+    
+    function remove_value_from_cookie(cookie_name, object){
+      value   = object.value
+      values  = $.cookie(cookie_name).split(' ');
+      values  = $.grep(values,  function(unmatched_value){return unmatched_value != value;});
+      $.cookie(cookie_name, values.join(' '));
+    };
+    
+    function cookie_defined(cookie_name){
+      if ($.cookie(cookie_name) != null){
+        return true
+      }else{return false}
+    };
+  
+  // Check the boxes that their values are in the cookies.
   $.each( $('input:checkbox[id]=ticket_to_report'), function(index, cb){
-    values  = $.cookie('tickets_to_report').split('');
-    if ($.inArray(cb.value, values) != -1 ){
-      cb.checked = true;
-    }
-  });
-  
-  function add_value_to_cookie(cookie_name, object){
-    if (cookie_defined(cookie_name) == true){     // Cookie Exists?
-        values  = $.cookie(cookie_name).split('');  // values to_array
-        values.push(object.value);                  // push value
-        values  = $.unique(values);                 // remove repetition
-        $.cookie(cookie_name, values.join(''));
-    } else{                                     // Initialize Cookie
-      $.cookie(cookie_name, object.value);      
-    }
-  };
-  
-  function remove_value_from_cookie(cookie_name, object){
-    value   = object.value
-    values  = $.cookie(cookie_name).split('');
-    values  = $.grep(values,  function(unmatched_value){return unmatched_value != value;});
-    $.cookie(cookie_name, values.join(''));
-  };
-  
-  function cookie_defined(cookie_name){
-    if ($.cookie(cookie_name) != null){
-      return true
-    }else{return false}
-  };
+    if(cookie_defined('tickets_to_report')==true){
+      values  = $.cookie('tickets_to_report').split(' ');
+      if ($.inArray(cb.value, values) != -1 ){
+        cb.checked = true;
+      };
+    };
+  });  
+
 });
