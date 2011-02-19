7da97b809ba53966514ef46b0b25e0a2940bd56d
Alan Andrade <gallito7@gmail.com>
2011-02-18 19:53:37 -0600

    Fixed bug with pdf Generation button

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -20,8 +20,12 @@ class TicketsController < ApplicationController
       end
       
       format.pdf  do
-        @tickets_to_report = Ticket.find(cookies['tickets_to_report'].split(' '))
-        send_data Ticket.to_pdf(@tickets_to_report)
+          if cookies['tickets_to_report'].nil?
+            redirect_to(tickets_path, :notice =>  "Debes seleccionar las quejas que quieres exportar") 
+          else
+            @tickets_to_report = Ticket.find(cookies['tickets_to_report'].split(' '))
+            send_data Ticket.to_pdf(@tickets_to_report), :display => 'inline'
+          end
       end
     end
   end
@@ -51,34 +55,19 @@ class TicketsController < ApplicationController
   end
 
   def create    
-    ## A student create a ticket
-    #@ticket = if @current_user.admin? and params[:ticket].has_key? :student_id
-    #            if params[:ticket][:student_id].empty?
-    #              Ticket.new(params[:ticket])
-    #            else
-    #              student = User.find_by_uid params[:ticket][:student_id]
-    #              student.tickets.build(params[:ticket])
-    #            end
-    #          else
-    #            @current_user.tickets.build(params[:ticket])
-    #          end
-    ## Almost 10 lines cutted into 2. :D
-    
     @ticket = Ticket.new(params[:ticket])
     @ticket.student = (User.find_by_uid(@ticket.student_id) or @current_user)
     @ticket.save ? 
       redirect_to(@ticket, :notice => 'Tu queja ha sido enviada') :
       render(:action => "new")
-
   end
 
   def update
-    @ticket = Ticket.find(params[:id]); @ticket.current_user = @current_user
-      if @ticket.update_attributes(params[:ticket])
-        redirect_to(@ticket, :notice => 'Se ha actualizado la queja con exito.')
-      else
-        render :action => "edit"
-      end
+    @ticket = Ticket.find(params[:id])
+    @ticket.current_user = @current_user
+    @ticket.update_attributes(params[:ticket]) ?
+      redirect_to(@ticket, :notice => 'Se ha actualizado la queja con exito.') :
+      render(:action => "edit")      
   end
 
   def destroy
