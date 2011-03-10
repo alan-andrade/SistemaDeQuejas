5e37c1b31c932ecd7b7fdb32d3030985a52731fa
Alan Andrade <gallito7@gmail.com>
2011-03-10 08:09:04 -0600

    Unobstrusive Javascript for Commenting in a change or ticket

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,12 @@ class CommentsController < ApplicationController
   before_filter :require_user
   
   def create
-    post      = Post.find(params[:post_id])
-    @comment  = post.comments.build(params[:comment])
+    @post     = Post.find(params[:post_id])
+    @comment  = @post.comments.build(params[:comment])
     @comment.responsible  = @current_user
     if @comment.save
       respond_to  do |format|
-        format.js{@comment}
+        format.js{}
         format.html{redirect_to ticket_path(@comment.ticket), :notice =>  "Comentado" }
       end
     end
